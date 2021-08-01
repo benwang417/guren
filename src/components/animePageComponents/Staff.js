2889de0af2b5fea428e94dba0e36a9b28a2709cc
Ben Wang <benwang@Bens-MacBook-Air.local>
2021-08-01 17:18:11 -0500

    added staff component with links to staff member  page

--- src/components/animePageComponents/Staff.js
+++ src/components/animePageComponents/Staff.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
-function Staff () {
+function Staff ({id}) {
+    const [staff, setStaff] = useState([])
+
+    useEffect(() => {
+        const getStaff = async () => {
+            const query = `
+                query ($id: Int) {
+                    Media (id: $id, type: ANIME) {
+                        title {
+                            english
+                        }
+                        staff (page: 1) {
+                            edges {
+                                node {
+                                    name {
+                                        first
+                                        last
+                                    }
+                                    id
+                                    image {
+                                        medium
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            `
+
+            const variables = {
+                id: id
+            }
+
+            const headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+
+            const response = await axios.post('https://graphql.anilist.co', {
+                query,
+                variables,
+                headers
+            })
+            setStaff(response.data.data.Media.staff.edges)
+        }
+
+        getStaff()
+    }, [])
+
+    const renderedStaff = staff.map((staff) => {
+        const firstName = staff.node.name.first
+        const lastName = staff.node.name.last
+
+        const url = lastName ? `/staff/${id}/${firstName}-${lastName}` : `/staff/${id}/${firstName}`
+        return (
+            <Link to={url} key={staff.node.id}>
+                <div>{firstName} {lastName}</div>
+            </Link>
+        )
+    })
+    console.log(staff)
+
+    if (!staff) {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
     return (
         <div>
-            staff
+            <h1>Staff</h1>
+            {renderedStaff}
         </div>
     )
 }
