893834700d0932cdd6d03389d3ed88d332297191
Ben Wang <benwang417@gmail.com>
2021-08-04 12:31:36 -0500

    added score input to add to list modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react'
 import axios from 'axios'
-import React from 'react'
 
+const watchStatus = ['CURRENT', 'PLANNED', 'COMPLETED', 'DROPPED', 'PAUSED', 'REPEATING']
 
 function Modal({show, setModalOpen}) {
     const token = localStorage.getItem('token')
+    const [score, setScore] = useState(0)
+    const [status, setStatus] = useState('COMPLETED')
+    
+    const scoreIsValid = () => {
+        if (typeof(score) !== 'number') {
+            setScore(0)
+        }
+    }
 
     const submit = async () => {
+        scoreIsValid() //check if score is num between 0-100
         const query = `
-                mutation ($mediaId: Int, $status: MediaListStatus) {
-                    SaveMediaListEntry (mediaId: $mediaId, status: $status) {
+                mutation ($mediaId: Int, $status: MediaListStatus, $score: Float) {
+                    SaveMediaListEntry (mediaId: $mediaId, status: $status, score: $score) {
                         id
                         status
                     }
@@ -17,7 +27,8 @@ function Modal({show, setModalOpen}) {
 
         const variables = {
             mediaId: show.id,
-            status: 'COMPLETED'
+            status: 'COMPLETED',
+            score: score
         }
 
         const headers = {
@@ -40,6 +51,10 @@ function Modal({show, setModalOpen}) {
     return (
         <div>
             modal
+            <input
+                value={score}
+                onChange={(e) => setScore(e.target.value)}
+            />
             <div onClick={() => setModalOpen(false)}>
                 close
             </div>
