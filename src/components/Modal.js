632e48dd341d60fa4ba61aa0ec4e1339188956be
Ben Wang <benwang417@gmail.com>
2021-08-04 17:16:35 -0500

    added portal element in index.html and made modal a portal to render on top of other elements

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,101 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import ReactDOM from 'react-dom'
 import axios from 'axios'
 import Dropdown from './Dropdown'
+import { UserContext } from '../UserContext'
 
 const watchStatus = ['CURRENT', 'PLANNED', 'COMPLETED', 'DROPPED', 'PAUSED', 'REPEATING']
+
+const MODAL_STYLES = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: 'white',
+    padding: '50px',
+    zIndex: 1000
+}
+
+const OVERLAY_STYLES = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, .7)',
+    zIndex: '1000'
+}
 
 function Modal({show, setModalOpen}) {
     const token = localStorage.getItem('token')
     const [score, setScore] = useState(0)
     const [progress, setProgress] = useState(0)
     const [statusSelection, setStatusSelection] = useState('COMPLETED')
-    
+    const [entryId, setEntryId] = useState(0)
+    const [userLists, setUserLists] = useState([])
+    const {user} = useContext(UserContext)
+
+    useEffect(() => {
+        if (!user) {
+            return
+        }
+        const getLists = async () => {
+            const query = `
+                query ($userId: Int){
+                    MediaListCollection (userId: $userId, type: ANIME) {
+                        lists {
+                            name
+                            status
+                            entries {
+                                media {
+                                title {
+                                    english
+                                }
+                                id
+                                coverImage {
+                                    medium
+                                    }
+                                }
+                                score
+                                progress
+                                status
+                            }
+                        }
+                    }
+                }
+            `
+
+            const variables = {
+                userId: user.id
+            }
+
+            const headers = {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+
+            const response = await axios.post('https://graphql.anilist.co', {
+                query,
+                variables
+            }, {
+                headers
+            })
+            setUserLists(response.data.data.MediaListCollection.lists)
+            
+        }
+
+        getLists()
+    }, [])
+
+    console.log(userLists)
     const scoreIsValid = () => {
         if (typeof(score) !== 'number') {
+            setScore(0)
+        }
+        if (score > 100) {
+            setScore(100)
+        }
+        if (score < 0) {
             setScore(0)
         }
     }
@@ -22,6 +106,9 @@ function Modal({show, setModalOpen}) {
         }
         if (progress > show.episodes) {
             setProgress(show.episodes)
+        }
+        if (progress < 0) {
+            setProgress(0)
         }
 
     }
@@ -40,7 +127,7 @@ function Modal({show, setModalOpen}) {
 
         const variables = {
             mediaId: show.id,
-            status: 'COMPLETED',
+            status: statusSelection,
             score: score
         }
 
@@ -60,38 +147,68 @@ function Modal({show, setModalOpen}) {
         setModalOpen(false)
     }
 
+    const deleteEntry = async () => {
+        const query = `
+                mutation ($id: Int) {
+                    DeleteMediaListEntry (id: $id) {
+                    }
+                }
+            `
+
+        const variables = {
+            id: entryId
+        }
+
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        
+        const response = await axios.post('https://graphql.anilist.co', {
+            query,
+            variables
+        }, {
+            headers
+        })
+        console.log(response)
+        setModalOpen(false)
+    }
+
     console.log(show)
-    return (
-        <div>
-            <h1>modal</h1>
-            <div>
-                score
-                <input
-                    value={score}
-                    onChange={(e) => setScore(e.target.value)}
-                />
-                <Dropdown 
-                        options={watchStatus} filterTitle='status' 
-                        selection={statusSelection} setSelection={setStatusSelection}
-                        canBeEmpty={false}
-                />
-                progress
-                <input
-                    value={progress}
-                    onChange={(e) => setProgress(e.target.value)}
-                />
-                / {show.episodes}
-            </div>
-            <div style={{display: 'flex'}}>
-                <div onClick={() => setModalOpen(false)}>
-                    cancel
+    return ReactDOM.createPortal(
+        <div style={OVERLAY_STYLES}>
+            <div style={MODAL_STYLES}>
+                <h1>modal</h1>
+                <div>
+                    score
+                    <input
+                        value={score}
+                        onChange={(e) => setScore(e.target.value)}
+                    />
+                    <Dropdown 
+                            options={watchStatus} filterTitle='status' 
+                            selection={statusSelection} setSelection={setStatusSelection}
+                            canBeEmpty={false}
+                    />
+                    progress
+                    <input
+                        value={progress}
+                        onChange={(e) => setProgress(e.target.value)}
+                    />
+                    / {show.episodes}
                 </div>
-                <button onClick={submit}>
-                    submit
-                </button>
+                <div style={{display: 'flex'}}>
+                    <div onClick={() => setModalOpen(false)}>
+                        cancel
+                    </div>
+                    <button onClick={submit}>
+                        submit
+                    </button>
+                </div>
             </div>
-            
-        </div>
+        </div>,
+        document.getElementById('portal')
     )
 }
 
