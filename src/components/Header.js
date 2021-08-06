9ef61320214995370541b95e601e7f312d1ed129
Ben Wang <benwang@Bens-MacBook-Air.local>
2021-08-05 21:26:55 -0500

    added mobile menu to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Header.css'
+import { FiMenu } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { ThemeContext } from '../ThemeContext'
 import { UserContext } from '../UserContext'
@@ -8,6 +9,7 @@ import { UserContext } from '../UserContext'
 function Header() {
     const {theme, setTheme} = useContext(ThemeContext)
     const {user} = useContext(UserContext)
+    const [burgerOpen, setBurgerOpen] = useState(false)
 
     const toggleTheme = (() => {
         if (theme === 'light') {
@@ -22,39 +24,45 @@ function Header() {
     }, [theme])
 
     return (
+        <div className='header'>
             <div className={`nav ${theme}`}>
                 <h4>
                     <Link to='/' className='logo'>guren | ぐれん</Link>
                 </h4>
-                <ul className='nav-links'>
-                    <li>
-                        <Link to='/' className={`link ${theme}`}>home</Link>
-                    </li>
-                    <li>
-                        <Link to='/anime' className={`link ${theme}`}>top anime</Link>
-                    </li>
-                    <li>
-                        <Link to='/search' className={`link ${theme}`}>search</Link>
-                    </li>
+                <div className='nav-links'>
+                    <Link to='/' className={`link ${theme}`}>home</Link>
+                    <Link to='/anime' className={`link ${theme}`}>top anime</Link>
+                    <Link to='/search' className={`link ${theme}`}>search</Link>
                     { !user ? 
-                    <li>
-                        <a className={`link ${theme}`} href='https://anilist.co/api/v2/oauth/authorize?client_id=6197&response_type=token'>login with AniList</a>
-                    </li> :
-                    <li>
-                        <Link to={`/user/${user.name}/animelist`} className={`link ${theme}`}>my list</Link>
-                    </li>
+                    <a className={`link ${theme}`} href='https://anilist.co/api/v2/oauth/authorize?client_id=6197&response_type=token'>login</a>
+                     :
+                    <Link to={`/user/${user.name}/animelist`} className={`link ${theme}`}>my list</Link>
                     }
                     { user ?
-                    <li>
-                        <img src={user.avatar.medium} style={{height: 'auto', width: '60px'}}/>
-                    </li> :
-                    <li></li>
+                    <img src={user.avatar.medium} style={{height: 'auto', width: '60px'}}/>
+                     : null
                     }
-                    <li>
-                        <button onClick={toggleTheme} className='themeButton'>{theme === 'light' ? 'dark mode' : 'light mode'}</button>
-                    </li>
-                </ul>
+                    <button onClick={toggleTheme} className='themeButton'>{theme === 'light' ? 'dark mode' : 'light mode'}</button>
+                </div>
+                <FiMenu className='burger' onClick={() => setBurgerOpen(!burgerOpen)} />
+                { user ?
+                <li>
+                    <img src={user.avatar.medium} style={{height: 'auto', width: '60px'}}/>
+                </li> : null
+                }
             </div>
+            {burgerOpen ? 
+            <div className='mobile-menu'>
+                    <Link to='/' className={`link ${theme}`}>home</Link>
+                    <Link to='/search' className={`link ${theme}`}>top anime</Link>
+                { !user ? 
+                    <a className={`link ${theme}`} href='https://anilist.co/api/v2/oauth/authorize?client_id=6197&response_type=token'>login with AniList</a>
+                 :
+                    <Link to={`/user/${user.name}/animelist`} className={`link ${theme}`}>my list</Link>
+                }
+            </div> : null
+            }
+        </div>
     )
 }
 
